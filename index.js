53351400ea254e2656c9a56949c7174430818737
duskvirkus <duskvirkus@protonmail.com>
2025-01-11 02:17:02 -0700

    cleanup

--- index.js
+++ index.js
@@ -10,34 +10,19 @@ let roomCounter = 0;
 const roomMatcher = new RegExp(/^[0-9]{1,19}$/);
 const TARGET_ROOM_SIZE = 2;
 
-const io = require('socket.io')(server//, {
-  // allowRequest: (req, cb) => {
-  //   console.log("hello")
-  //   console.log(req.headers)
-  //   if (req.headers.host === "localhost:8080") {
-  //     cb(null, true);
-  //   }
-  //   // const isAllowed = req.headers.origin === 'http://good.com';
-  //   cb(null, false);
-  // }
-// }
-);
+const io = require('socket.io')(server);
 io.on('connection', socket => {
 
   console.log(`${socket.id} is connected`);
 
   let room = joinRoom(socket);
-  console.log(`room ${room}`);
   socket.emit('established');
 
   socket.on('move', data => {
-    console.log(data);
     io.to(room).emit('player-move', data);
   });
 
   socket.on('disconnect', () => {
-    // console.log(room + ' had a lost connection');
-    // io.to(room).emit('connection-lost');
     console.log(`${socket.id} was disconnected`);
   });
   
@@ -56,11 +41,8 @@ function joinRoom(socket) {
 
 function roomToJoin() {
   let roomList = getRoomList();
-  console.log(`room list ${roomList}`)
   let keys = Object.keys(roomList);
-  console.log(`room list keys ${keys}`)
   for (let i = 0; i < keys.length; i++) {
-    console.log(`roomList[keys[i]] ${roomList[keys[i]]}`)
     if (roomList[keys[i]].size < TARGET_ROOM_SIZE) {
       return keys[i];
     }
@@ -70,35 +52,20 @@ function roomToJoin() {
 
 function getRoomList() {
   let allRooms = io.sockets.adapter.rooms;
-  console.log(allRooms.keys())
-  // console.log(Object.keys(allRooms))
   let keys = allRooms.keys().filter(key => key.match(roomMatcher));
-  console.log(`keys ${keys}`);
   let rooms = {};
   keys.forEach(key => {
-    console.log(key)
     rooms[key] = allRooms.get(key)
   })
-  // for (let key : keys) {
-  //   console.log(key);
-  // }
-  // for (let i = 0; i < keys.length; i++) {
-  //   console.log(`${i} ${keys[i]}`)
-  //   rooms[keys[i]] = allRooms.get(keys[i]);
-  // }
-  console.log(rooms)
   return rooms;
 }
 
 function checkRoomSize(room) {
-  console.log(io.sockets.adapter.rooms)
   if (io.sockets.adapter.rooms.get(room).size === TARGET_ROOM_SIZE) {
-    console.log(io.sockets.adapter.rooms.get(room).keys())
     let socketsInRoom = io.sockets.adapter.rooms.get(room).keys();
 
     let index = 0;
     socketsInRoom.forEach(socket => {
-      // console.log(`sockets connected ${io.sockets.connected.get(socket)}`)
       io.to(socket).emit('room-ready', {
         socketIDs: socketsInRoom,
         idInRoom: index,
@@ -106,30 +73,7 @@ function checkRoomSize(room) {
       });
       index++;
     })
-
-    // console.log(socketsInRoom)
-    // console.log()
-
-    // for (let i = 0; i < socketsInRoom.length; ++i) {
-    //   payload = {
-    //     socketIDs: socketsInRoom,
-    //     idInRoom: i,
-    //     gameStartTime: Date.now() + 6000,
-    //   };
-    //   console.log(payload)
-    //   io.to(socket).emit('room-ready', payload);
-    // }
-    // console.log(`socketsInRoom ${socketsInRoom}`)
-    // for (let i = 0; i < socketsInRoom.length; i++) {
-    //   console.log(`socketInRoom[${i}] ${socketsInRoom[i]}`)
-    //   io.sockets.connected[socketsInRoom[i]].emit('room-ready', {
-    //     socketIDs: socketsInRoom,
-    //     idInRoom: i,
-    //     gameStartTime: Date.now() + 6000,
-    //   });
-    // }
-    console.log(room + ' is full');
-    console.log(io.sockets.adapter.rooms.get(room));
+    console.log(`room is full ${room}`)
   }
 }
 
--- public/online/online.js
+++ public/online/online.js
@@ -30,14 +30,7 @@ function setup() {
 }
 
 function setupSocket() {
-  // socket = io('localhost:8080', //{
-  // //   withCredentials: true,
-  // //   extraHeaders: {
-  // //     "my-custom-header": "abcd"
-  // //   }
-  // // }
-  // );
-  socket = io('localhost:8080')
+  socket = io('https://clownfish-app-gft3y.ondigitalocean.app')
 
   socket.on('established', () => {
     updateStatus('Waiting for another player ...<br>Invite a friend or you may be here awhile.');
